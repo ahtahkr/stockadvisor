bb4ae29dac20bfa9f5f1d36747471ec53540a6b7
Ashit Adhikari <as.adhikari@outlook.com>
2018-02-12 10:57:41 -0600

    Windows Service to receive stock data from alphavantage updated.

--- Web_Development/WindowsService/Jobs/Update_Share.cs
+++ Web_Development/WindowsService/Jobs/Update_Share.cs
@@ -16,16 +16,14 @@ namespace AustinsFirstProject.StockAdvisor.WindowsService
 {
     public partial class Service1 : ServiceBase
     {
-        private void Update_Share(object sender = null, ElapsedEventArgs e = null)
+        private void Update_Share_AlphaVantage(object sender = null, ElapsedEventArgs e = null)
         {
             List<Ticker_Class> tk = null;
             string result = "Before TRY.";
-            string log_file_name = "";
+            string log_file_name = "Share_Updated_AlphaVantage";
 
             try
             {
-                Logger.Log("AustinsFirstProject.StockAdvisor.WindowsService.Update_Shares", log_file_name);
-
                 DateTime current_time = DateTime.Now;
                 int current_hour_min = (current_time.Hour * 60) + current_time.Minute;
                 
@@ -37,26 +35,12 @@ namespace AustinsFirstProject.StockAdvisor.WindowsService
 
                 int hour_min = (hour * 60) + minute;
 
-                if (log == "true")
-                {
-                    string logger = "Hour: [" + hour + "] | Minute: [" + minute + "] | Log: [" + log + "] | ApiKey: [" + apikey + "] | Process: [" + date_to_process + "] | HourMin: [" + hour_min + "]";
-                    Logger.Log(logger, log_file_name);
-                }
-
                 if (current_hour_min >= hour_min)
                 {
-                    if (log == "true")
-                    {
-                        Logger.Log("Time to Rock.", log_file_name);
-                    }
                     tk = Library.Intrinio.Utility.Database.Get_Tickers();
 
                     if (tk.Count > 0)
                     {
-                        if (log == "true")
-                        {
-                            Logger.Log("Ticker: " + tk[0].Ticker, log_file_name);
-                        }
                         List<string> date_list = new List<string>();
 
                         if (date_to_process == "today")
@@ -64,11 +48,6 @@ namespace AustinsFirstProject.StockAdvisor.WindowsService
                             date_list.Add(DateTime.Now.ToString("yyyy-MM-dd"));
                         }
                         result = TIME_SERIES_DAILY.GET(tk[0].Ticker, apikey, date_list);
-
-                        if (log == "true")
-                        {
-                            Logger.Log("Result: " + result, log_file_name);
-                        }
 
                         List<Share> shares = JsonConvert.DeserializeObject<List<Share>>(result);
                         
@@ -85,31 +64,8 @@ namespace AustinsFirstProject.StockAdvisor.WindowsService
                         {
                             DatabaseUpdateHelper.Update_WebApi_AlphaVantage(tk[0].Ticker, 0);
                         }
-
-                        if (log == "true")
-                        {
-                            Logger.Log("Share successfully processed for " + shares.Count + " shares.", log_file_name);
-                        }
-
-                        Logger.Log("Ticker: [" + tk[0].Ticker + "] Shares: [" + shares.Count + "].", log_file_name);
-                        
-                    }
-                    else
-                    {
-                        if (log == "true")
-                        {
-                            //eventLog_update_share(DateTime.Now + " : " + );
-                            //eventLog_update_share.WriteEntry(DateTime.Now + " : " + "No Tickers to process.");
-                            Logger.Log("No Tickers to process.", log_file_name);
-                        }
-                    }
-                } else
-                {
-                    if (log == "true")
-                    {
-                        //eventLog_update_share.WriteEntry(DateTime.Now + " : " + "Not a good time to run the process. system_hour = " + current_time.Hour + " config_file_hour = " + hour + ". system_minute = " + current_time.Minute + " config_file_minute = " + minute);
-                        Logger.Log("Not a good time to run the process. system_hour = " + current_time.Hour + " config_file_hour = " + hour + ". system_minute = " + current_time.Minute + " config_file_minute = " + minute, log_file_name);
-                    }
+                        Logger.Log(tk[0].Ticker + "," + shares.Count, log_file_name);                        
+                    }                    
                 }
             } catch (Exception ex)
             {
