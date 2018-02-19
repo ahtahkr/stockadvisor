0b0a12dee1bb0bfbb63e5304b5a7392c0a0a2eaa
Austin Adhikari - Desktop <as.adhikari@outlook.com>
2018-02-18 19:19:24 -0600

    New app 'Git_Info_Accessor' added to the list.

---
+++ Git_Info_Accessor/Git_Info_Accessor/LibGit2Sharp/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LibGit2Sharp
+{
+    public class Class1
+    {
+    }
+}
---
+++ Git_Info_Accessor/Git_Info_Accessor/Tester/Program.cs
@@ -0,0 +1,35 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var repo = new Repository("https://github.com/trilinos/Trilinos.git"))
+            {
+                var RFC2822Format = "ddd dd MMM HH:mm:ss yyyy K";
+
+                foreach (Commit c in repo.Commits.Take(15))
+                {
+                    Console.WriteLine(string.Format("commit {0}", c.Id));
+
+                    if (c.Parents.Count() > 1)
+                    {
+                        Console.WriteLine("Merge: {0}",
+                            string.Join(" ", c.Parents.Select(p => p.Id.Sha.Substring(0, 7)).ToArray()));
+                    }
+
+                    Console.WriteLine(string.Format("Author: {0} <{1}>", c.Author.Name, c.Author.Email));
+                    Console.WriteLine("Date:   {0}", c.Author.When.ToString(RFC2822Format, CultureInfo.InvariantCulture));
+                    Console.WriteLine();
+                    Console.WriteLine(c.Message);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
