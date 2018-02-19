9dd578bcee17d62f61faef01b7643adb7065697a
Ashit Adhikari <as.adhikari@outlook.com>
2018-02-19 14:45:09 -0600

    New app to access online Git repositories info added.

---
+++ Web_Development/Git_Info_Accessor/Areas/Repository/Controllers/RepositoryController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AustinsFirstProject.Git_Info_Accessor.Areas.Repository.Controllers
+{
+    [Area("Repository")]
+    [Route("")]
+    [Route("Repository")]
+    public class RepositoryController : Controller
+    {
+        [Route("")]
+        public void ReRoute()
+        {
+            string baseUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+            Response.Redirect(baseUrl + "/Repository/Index");
+        }
+
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            string result = Github_Api.Api.Rest_Api_V3.Repositories.Get_Basic_Info("bwillis", "jekyll-github-sample");
+
+            Github_Api.Model.Repository repository = new Github_Api.Model.Repository();
+            if (String.IsNullOrEmpty(result)) { }
+            else
+            {
+                try
+                {
+                    repository = JsonConvert.DeserializeObject<Github_Api.Model.Repository>(result);
+                }
+                catch (Exception ex)
+                {
+                    repository = new Github_Api.Model.Repository();
+                }
+            }
+
+            return View(repository);
+        }
+    }
+}
---
+++ Web_Development/Github_Api/Repositories.cs
@@ -0,0 +1,46 @@
+﻿using AustinsFirstProject.Library;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Octokit;
+using System;
+
+namespace AustinsFirstProject.Github_Api.Api
+{
+    public static class Rest_Api_V3
+    {
+        private const string API_URL = "https://api.github.com";
+        private const string APP_NAME = "GIT_INFO_ACCESSOR";
+        private const string GitHub_Api_Token = "f1a471df899699b74ad6cbc021b5cb6d9a963908";
+
+        public static class Repositories
+        {
+            /// <summary>
+            /// https://api.github.com/repos/{owner}/{repo}
+            /// </summary>
+            public static string Get_Basic_Info(string _owner, string _repo, string token = "")
+            {
+                string result;
+                try
+                {
+                    var client = new GitHubClient(new ProductHeaderValue(APP_NAME));
+
+                    if (!String.IsNullOrEmpty(token))
+                    {
+                        var tokenAuth = new Octokit.Credentials(token);
+                        client.Credentials = tokenAuth;
+                    }
+
+                    result = JsonConvert.SerializeObject(client.Repository.Get(_owner, _repo));
+                    dynamic dynamic = JObject.Parse(result);
+                    result = dynamic.Result.ToString();
+
+                } catch (Exception ex)
+                {
+                    Logger.Log_Error("AustinsFirstProject.Github_Api.Api.Rest_Api_V3.Repositories.Get_Basic_Info failed. Error Msg: " + ex.Message);
+                    result = "";
+                }
+                return result;
+            }
+        }
+    }
+}
