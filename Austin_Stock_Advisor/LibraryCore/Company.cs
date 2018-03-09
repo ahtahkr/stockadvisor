742a78c2fc0c3457a97ac09e29f62c4189bd6ca6
Ashit Adhikari - Home Desktop <as.adhikari@outlook.com>
2018-03-09 16:15:58 -0600

    New Core Library Project for the Stock Advisor started.

---
+++ Austin_Stock_Advisor/LibraryCore/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AustinStockAdvisor.LibraryCore
+{
+    public class Company
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public bool isEnabled { get; set; }
+        public string Type { get; set; }
+        public int IexId
+        {
+            get; set;
+        }
+    }
+}
