548c92075ec700b4272a4bc1dcb401175d905b1f
Austin Adhikari - Desktop <as.adhikari@outlook.com>
2018-03-12 02:51:05 -0500

    Jquery button added to check and uncheck robinhood.

--- Austin_Stock_Advisor/Austin_Stock_Advisor/Controllers/CompanyController.cs
+++ Austin_Stock_Advisor/Austin_Stock_Advisor/Controllers/CompanyController.cs
@@ -29,5 +29,21 @@ namespace AustinStockAdvisor.Controllers
             List<Library.Company> _companies = JsonConvert.DeserializeObject<List<Library.Company>>(companies);
             return View(_companies);
         }
+
+        [Route("[action]")]
+        [HttpGet]
+        public string Company_Alter_Robinhood(string symbol)
+        {
+            configRoot = ConfigurationHelper.GetConfiguration(Directory.GetCurrentDirectory());
+            string connection_string = configRoot.GetConnectionString(configRoot.GetSection("environmentVariables")["ENVIRONMENT"]);
+
+            Dictionary<string, object> param = new Dictionary<string, object>();
+            param.Add("Symbol", symbol);
+
+            return AustinStockAdvisor.Library.Database.ExecuteProcedure.Get(
+                "[fsn].[Company_Alter_Robinhood]"
+                , connection_string, param);
+        }
     }
 }
+            
--- Austin_Stock_Advisor/Austin_Stock_Advisor/Startup.cs
+++ Austin_Stock_Advisor/Austin_Stock_Advisor/Startup.cs
@@ -34,7 +34,7 @@ namespace AustinStockAdvisor
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseMvc().UseStaticFiles();
         }
     }
 }
