b6adb1fd3464fde869993f0c0293ff5de5737c61
Austin Adhikari - Desktop <as.adhikari@outlook.com>
2018-03-22 21:12:52 -0500

    New Logging format added.

--- Austin_Stock_Advisor/Stock_Advisor_Window_Service/Jobs/IEXTrading_Get_Chart_3M.cs
+++ Austin_Stock_Advisor/Stock_Advisor_Window_Service/Jobs/IEXTrading_Get_Chart_3M.cs
@@ -13,6 +13,8 @@ namespace AustinStockAdvisor.WindowsService
     {
         private void IEXTrading_Get_Chart_Range(object sender = null, ElapsedEventArgs e = null)
         {
+            string log = "IEXTrading_Get_Chart_Range started.";
+
             string run = ConfigurationManager.AppSettings["IEXTrading_Get_Chart_Range"];
 
             if (run.Equals("0"))
@@ -24,16 +26,21 @@ namespace AustinStockAdvisor.WindowsService
             string symbol = "beforetry";
             string result = "beforetry";
             string webapi = "beforetry";
+            string range = "beforetry";
+
             List<AustinStockAdvisor.Library.Share> shares = new List<AustinStockAdvisor.Library.Share>();
             try
             {
                 result = AustinStockAdvisor.Library.Database.ExecuteProcedure.Get(
                 "[fsn].[Company_Get_Symbol_ChartRange]", connection_string);
 
+                log = "Got company from db: " + result;
+
                 /* MethodFullName. */
                 string methodfullname = "[" + this.GetType().FullName + "." + System.Reflection.MethodBase.GetCurrentMethod().Name + "]";
 
                 if (String.IsNullOrEmpty(result) || result.Equals("[]")) {
+                    log = "Not Getting data for result: [" + result + "]";
                     Library.Logger.Log("Not Getting data for result: [" + result + "]", "IEXTrading_Get_Chart_Range");
                 }
                 else
@@ -42,12 +49,17 @@ namespace AustinStockAdvisor.WindowsService
                     dynamic stuff1 = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
                     symbol = stuff1[0].Symbol;
 
-                    Library.Logger.Log("Got data for "+ symbol +" result: [" + result + "]", "IEXTrading_Get_Chart_Range");
+                    range = stuff1[0].Range;
 
-                    string range = stuff1[0].Range;
+                    log = "Getting data for symbol: " + symbol + " range: " + range;
+
                     webapi = AustinStockAdvisor.IEXTrading.WebApi.V_1.Chart(symbol, range);
+
+                    log = "symbol: " + symbol + " range: " + range + " webapi: " + webapi;
+
                     shares = JsonConvert.DeserializeObject<List<AustinStockAdvisor.Library.Share>>(webapi);
 
+                    log = "symbol: " + symbol + " range: " + range + " shares: " + JsonConvert.SerializeObject(shares);
 
                     if (shares.Count > 0)
                     {
@@ -57,17 +69,23 @@ namespace AustinStockAdvisor.WindowsService
                             shares[a].Share_Insert_Update(connection_string);
                         }
 
+                        log = "symbol: " + symbol + " range: " + range + " shares inserted: " + shares.Count;
+
                         AustinStockAdvisor.Library.Company company = new AustinStockAdvisor.Library.Company();
                         company.Symbol = shares[0].Symbol;
                         company.Company_Update_IEX_Chart_3M(connection_string);
+
+                        log = "symbol: " + symbol + " range: " + range + " shares inserted and IEX_Chart_3m updated.";
+
                     } else
                     {
-                        //Library.Logger.Log("result [" + result + "] symbol: [" + symbol + "] webapi: [" + webapi + "]", methodfullname);
-
-                        Library.Logger.Log("Didn't got data for result: [" + result + "]", "IEXTrading_Get_Chart_Range");
+                        log = "symbol: " + symbol + " range: " + range + " shares.count: " + shares.Count;
+                        
                         AustinStockAdvisor.Library.Company company = new AustinStockAdvisor.Library.Company();
                         company.Symbol = symbol;
                         company.Company_Alter_IEX_Trading(connection_string);
+
+                        log = "symbol: " + symbol + " range: " + range + " IEX_Trading in Company table updated (not in catch).";
                     }
                 }
             
@@ -78,12 +96,15 @@ namespace AustinStockAdvisor.WindowsService
                 string methodfullname = "[" + this.GetType().FullName + "." + System.Reflection.MethodBase.GetCurrentMethod().Name + "]";
                 Library.Logger.Log_Error(methodfullname + " result ["+result+"] symbol:["+symbol+"] webapi: ["+webapi+"] Error Msg: " + ex.Message + ". Shares List: ["+ JsonConvert.SerializeObject(shares)+"]" );
 
+                log = "symbol: " + symbol + " range: " + range + " (in catch).";
+
                 AustinStockAdvisor.Library.Company company = new AustinStockAdvisor.Library.Company();
                 company.Symbol = symbol;
                 company.Company_Alter_IEX_Trading(connection_string);
 
-                return;
+                log = "symbol: " + symbol + " range: " + range + " IEX_Trading in Company table updated (in catch).";
             }
+            Library.Logger.Log(log, "IEXTrading_Get_Chart_Range");
         }
 
         private void IEXTrading_Market_Previous(object sender = null, ElapsedEventArgs e = null)
@@ -114,6 +135,7 @@ namespace AustinStockAdvisor.WindowsService
                         SHARE = JsonConvert.DeserializeObject<AustinStockAdvisor.Library.Share>('{' + _shares[0] + '}');
                         SHARE.Share_Insert_Update(connection_string);
                     }
+                    Library.Logger.Log("Got data for " + dt.DayOfWeek.ToString() + " on " + dt.Hour + " UTC", "IEXTrading_Market_Previous");
                 }
                 else
                 {
