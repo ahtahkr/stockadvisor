bd1721ed9d602376b1943c1f54578b88b5c3aedd
Ashit Adhikari <aadhikari@kenall.com>
2018-04-11 10:29:38 -0500

    IEXTrading WebApi Previous added.

--- IEXTrading/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IEXTrading.WebApi
-{
-    public class Api_1
-    {
-    }
-}
---
+++ IEXTrading/WebApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IEXTrading.WebApi
+{
+    public static class Api_1
+    {
+        private const string URI = "https://api.iextrading.com/1.0/";
+
+        public static string Previous(string symbol = "market")
+        {
+            return Library.HttpRequestUtility.GetRequest(URI + "stock/" + symbol + "/previous");
+        }
+    }
+}
