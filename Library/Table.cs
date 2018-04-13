ee2c0baa69fdd11aa6d8fb50b41095c21dc0e828
Austin Adhikari <as.adhikari@outlook.com>
2018-04-13 10:02:11 -0500

    Convert_to_CSV_String added to the Library that converts DataTable object to a CSV style string.

---
+++ Library/Table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public static class Table
+    {
+        public static string Convert_to_CSV_String(DataTable _table)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            IEnumerable<string> columnNames = _table.Columns.Cast<DataColumn>().
+                                              Select(column => column.ColumnName);
+            sb.AppendLine(string.Join(",", columnNames));
+
+            foreach (DataRow row in _table.Rows)
+            {
+                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                sb.AppendLine(string.Join(",", fields));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
